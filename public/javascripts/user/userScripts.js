6629dd54b27dd959249939472b25d7dfcefd5f53
João Pedro Salgado Pereira <8220102@estg.ipp.pt>
2024-04-12 21:42:57 +0100

    chore(user-form): Creation of the User registration form

---
+++ public/javascripts/user/userScripts.js
@@ -0,0 +1,27 @@
+document.querySelector('#inputPassword + .input-group-append .reveal').addEventListener('click', function (e) {
+    const passwordInput = document.querySelector('#inputPassword');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    const icon = this.querySelector('i');
+    if (type === 'text') {
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+});
+
+document.querySelector('#inputRepeatPassword + .input-group-append .reveal').addEventListener('click', function (e) {
+    const passwordInput = document.querySelector('#inputRepeatPassword');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    const icon = this.querySelector('i');
+    if (type === 'text') {
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+});
