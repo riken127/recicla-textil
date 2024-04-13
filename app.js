0127594b355dd01d0b6c670abbb5483c2bc9311c
Lucas <7lucas7fernandes77@gmail.com>
2024-04-13 15:48:09 +0100

    chore(login): create login view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 var app = express();
 var mongoose = require('mongoose');
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
