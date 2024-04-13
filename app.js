fe4bb4007316b20a35a382eccd69f96aa6d243c3
Lucas <7lucas7fernandes77@gmail.com>
2024-04-13 14:40:44 +0100

    chore(benefactor): creation of benefactor form

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var benefactorRouter = require('./routes/benefactor/form');
+
 var app = express();
 var mongoose = require('mongoose');
 
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/benefactor/form', benefactorRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
