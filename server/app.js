d3b46d018bfb8b7ca35b90a98b2d086c48332c62
Lucas <7lucas7fernandes77@gmail.com>
2024-05-13 16:22:55 +0100

    chore(swagger): Swagger configuration

--- server/app.js
+++ server/app.js
@@ -1,37 +1,39 @@
-require('dotenv').config();
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var usersRouter = require('./routes/users');
-var donationRouter = require('./routes/donations');
-var benefactorRouter = require('./routes/benefactors');
-var dashboardRouter = require('./routes/dashboard');
-var homeRouter = require('./routes/home');
-var authRouter = require('./routes/auth');
-var mongoose = require('mongoose');
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var usersRouter = require("./routes/users");
+var donationRouter = require("./routes/donations");
+var benefactorRouter = require("./routes/benefactors");
+var dashboardRouter = require("./routes/dashboard");
+var homeRouter = require("./routes/home");
+var authRouter = require("./routes/auth");
+var mongoose = require("mongoose");
 var app = express();
+const swaggerUI = require("swagger-ui-express");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use('/uploads', express.static('./uploads'))
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use("/uploads", express.static("./uploads"));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(require("./swagger")));
 
-app.use('/users', usersRouter);
-app.use('/donations', donationRouter);
-app.use('/benefactors', benefactorRouter);
-app.use('/dashboard', dashboardRouter);
-app.use('/home', homeRouter);
-app.use('/auth', authRouter);
+app.use("/users", usersRouter);
+app.use("/donations", donationRouter);
+app.use("/benefactors", benefactorRouter);
+app.use("/dashboard", dashboardRouter);
+app.use("/home", homeRouter);
+app.use("/auth", authRouter);
 app.use((req, res, next) => {
-    if (req.originalUrl === '/') {
-        res.redirect('/auth/login');
+    if (req.originalUrl === "/") {
+        res.redirect("/auth/login");
     } else {
         next();
     }
@@ -42,24 +44,25 @@ app.use((req, res, next) => {
     next(createError(404));
 });
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
-        console.log('MongoDB Connected successfully.');
+        console.log("MongoDB Connected successfully.");
     })
     .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
+        console.error("Error connecting to MongoDB:", error);
     });
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 
 module.exports = app;
---
+++ server/swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'A simple Express API',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    servers: [{ url: `http://localhost:3000` }],
+  },
+  apis: ['./routes/*.js', './models/*.js'],
+};
+
+module.exports = swaggerJsDoc(options);
